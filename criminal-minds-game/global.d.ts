d055fcf5da524e30f6b98db22d6cd83dbcad3ba4
Thamires Fernandes Mendes <thamiresfernandesm@gmail.com>
2025-07-31 10:37:12 -0300

    🛑 Remove workflows problemáticos - Deploy manual\n\n- Remove pasta .github/workflows/ que causava erros de permissão\n- Adiciona node_modules ao .gitignore\n- GitHub Pages funcionará via configuração manual\n- Branch gh-pages já está pronta para deploy\n- Solução mais simples e confiável

--- criminal-minds-game/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@testing-library/jest-dom" />
-/// <reference types="@types/testing-library__jest-dom" />
-
-// Importar jest-dom para estender os matchers do Jest
-import '@testing-library/jest-dom'
-
-export {} 
